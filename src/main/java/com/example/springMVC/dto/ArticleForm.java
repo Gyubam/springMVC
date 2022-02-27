79943b02e355c24ad44ea07d28bde6fa2ddeb632
gyubum <sgb8170@naver.com>
2022-02-27 18:24:26 +0900

    Rest Api 통신 구현

--- src/main/java/com/example/springMVC/dto/ArticleForm.java
+++ src/main/java/com/example/springMVC/dto/ArticleForm.java
@@ -6,19 +6,28 @@
 package com.example.springMVC.dto;
 
 import com.example.springMVC.entity.Article;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 public class ArticleForm {
 
+    @JsonProperty("id")
     private Long id;
+
+    @JsonProperty("title")
     private String title;
+
+    @JsonProperty("content")
     private String content;
 
     public Article toEntity() {
         return new Article(id, title, content);
     }
+
 
 }
